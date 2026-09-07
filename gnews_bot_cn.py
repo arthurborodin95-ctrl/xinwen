217102db8274f2327e66155e4ed66bbef22f9c56
arthurborodin95-ctrl <arthur.borodin95@gmail.com>
2026-09-07 09:08:24 +0300

    Update gnews_bot_cn.py

--- gnews_bot_cn.py
+++ gnews_bot_cn.py
@@ -52,18 +52,67 @@ CHAT_IDS = [int(TELEGRAM_CHAT_ID)]  # пока только один
 
 # ---- RSS ЛЕНТЫ ----
 RSS_FEEDS = [
-    "https://ria.ru/export/rss2/index.xml",
-    "https://tass.ru/rss/v2.xml",
-    "https://www.interfax.ru/rss.asp",
-    "https://www.kommersant.ru/RSS/news.xml",
-    "https://www.vedomosti.ru/rss",
-    "https://iz.ru/xml/rss/all.xml",
-    "https://www.tks.ru/law.rss",
-    "https://www.tks.ru/nearby.rss",
-    "https://www.infranews.ru/feed/",
-    "https://www.cnews.ru/news/rss",
-    "http://www.cbr.ru/rss/RssNews",
-    "http://www.cbr.ru/rss/RssPress",
+    # ===== Основные СМИ =====
+    "https://ria.ru/export/rss2/index.xml",              # РИА Новости
+    "https://tass.ru/rss/v2.xml",                        # ТАСС
+    "https://www.interfax.ru/rss.asp",                   # Интерфакс
+    "https://www.finmarket.ru/export/rss.asp",           # Финмаркет
+    "https://www.kommersant.ru/RSS/news.xml",            # Коммерсантъ
+    "https://www.vedomosti.ru/rss",                      # Ведомости
+    "https://1prime.ru/export/rss.xml",                  # Прайм
+    "https://www.forbes.ru/rss/all",                     # Forbes Russia
+    "https://iz.ru/xml/rss/all.xml",                     # Известия
+    "https://www.rbc.ru/rss/",                           # РБК
+    "https://lenta.ru/rss",                              # Lenta.ru
+    "https://www.gazeta.ru/export/rss/first.xml",        # Газета.ru
+    "https://expert.ru/rss/",                            # Эксперт
+    "https://www.fin-gazeta.ru/rss/",                    # Финансовая газета
+    "https://www.vestifinance.ru/rss",                   # Вести Финанс
+    "https://rg.ru/rss/",                                # Российская газета
+    "https://www.pnp.ru/rss/",                           # Парламентская газета
+
+    # ===== Специализированные (экономика, ВЭД, логистика) =====
+    "https://www.tks.ru/law.rss",                        # TKS.ru (законодательство)
+    "https://www.tks.ru/nearby.rss",                     # TKS.ru (смежные темы)
+    "https://trans.ru/rss/news",                         # Trans.ru (логистика)
+    "https://www.infranews.ru/feed/",                    # Infranews
+    "https://www.tourdom.ru/rss/",                       # Tourdom (туризм)
+    "https://www.autostat.ru/export/rss/",               # Автостат
+    "https://morvesti.ru/rss/",                          # Морские вести
+    "https://portnews.ru/rss/",                          # Portnews
+    "https://seanews.ru/feed/",                          # Seanews
+    "https://primpress.ru/rss/",                         # Primpress
+    "https://www.cnews.ru/news/rss",                     # CNews
+    "https://www.comnews.ru/rss",                        # ComNews
+    "https://www.ixbt.com/export/news.rss",              # IXBT (технологии)
+    "https://biang.ru/rss/",                             # Biang.ru
+    "https://www.eastrussia.ru/feed/",                   # EastRussia
+    "https://bigasia.ru/feed/",                          # BigAsia
+    "https://tvbrics.com/feed/",                         # TV BRICS
+    "https://infobrics.org/rss/",                        # BRICS Business
+    "https://eec.eaeunion.org/rss/",                     # ЕАЭС / ЕЭК
+
+    # ===== Китай и Азия =====
+    "http://russian.news.cn/rss/news.xml",               # Синьхуа (русская версия)
+    "http://russian.china.org.cn/rss/feed.xml",          # Китайский инфоцентр
+    "http://russian.people.com.cn/rss/feed.xml",         # People's Daily
+    "https://russian.china.org.cn/rss/business.xml",     # Китайский бизнес
+    "https://www.scmp.com/rss/",                         # South China Morning Post
+    "https://rsshub.app/cnbc/rss/",                      # CNBC (через RSSHub)
+
+    # ===== Официальные государственные =====
+    "http://www.cbr.ru/rss/RssNews",                     # ЦБ РФ (новости)
+    "http://www.cbr.ru/rss/RssPress",                    # ЦБ РФ (пресс-релизы)
+    "https://www.economy.gov.ru/rss",                    # Минэкономразвития
+    "https://minpromtorg.gov.ru/rss/",                   # Минпромторг
+    "http://www.customs.ru/rss/",                        # ФТС РФ
+    "https://www.ved.gov.ru/rss/",                       # Журнал "Внешняя торговля"
+
+    # ===== Логистика и промышленность =====
+    "https://www.logistics.ru/rss",                      # Логистика
+    "https://www.rzd-partner.ru/rss/",                   # РЖД-Партнёр
+    "https://www.stanok.info/rss/",                      # Станкостроение
+    "https://www.roprom.ru/rss/",                        # Российская промышленность
 ]
 MAX_ARTICLES_PER_FEED = 20
 
